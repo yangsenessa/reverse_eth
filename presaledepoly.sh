ec2e064d5f3a24097c9af8373ee981cd073b6be9
essa <yang82101@163.com>
2025-03-06 20:47:53 +0800

    delpoy

--- presaledepoly.sh
+++ presaledepoly.sh
@@ -1,5 +1,5 @@
 >presaledeploy.log
-truffle migrate -f 4 --to 4 --reset --verbose-rpc --network ganache >presaledeploy.log
+npx truffle migrate  --compile-non -f 4 --to 4 --reset --verbose-rpc --network mainnet >presaledeploy.log
 git add presaledeploy.log
 git commit -m "Deploy presale contract"
 git push
--- vestingdeploy.sh
+++ vestingdeploy.sh
@@ -1,5 +1,5 @@
 >vestingdeploy.log
-truffle migrate -f 3 --to 3 --reset --verbose-rpc --network ganache >vestingdeploy.log
+npx truffle migrate --compile-non -f 3 --to 3 --reset --verbose-rpc --network mainnet >vestingdeploy.log
 git add vestingdeploy.log
 git commit -m "Deploy vesting"
 git push
